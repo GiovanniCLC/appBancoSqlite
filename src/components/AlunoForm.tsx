4e6446de03240781997313ddae581a52f039561d
GIOVANNI CAETANO LIBERATO CASARETTO <202351050735@alunos.newtonpaiva.edu.br>
2026-05-13 19:57:22 -0300

    Atualiza AlunoForm

--- src/components/AlunoForm.tsx
+++ src/components/AlunoForm.tsx
@@ -19,3 +19,29 @@ export function AlunoForm({nome, email, onChangeNome, onChangeEmail, onSubmit}:P
        </View> 
     )
 }
+
+const styles=StyleSheet.create({
+input:{
+    borderWidth:1,
+    borderColor:"#8e8989",
+    borderRadius:5,
+    padding:10,
+    marginBottom:10,
+    fontSize:16,
+},
+
+botao:{
+    backgroundColor:"#640839",
+    padding:14,
+    borderRadius:8,
+    alignItems:"center",
+    marginBottom:20,
+},
+
+textobotao:{
+    color:"#F50909",
+    fontSize:16,
+    fontWeight:"700",
+},
+
+})
