123261008934f1c7145e8b7a7d2e8efa23dad499
GIOVANNI CAETANO LIBERATO CASARETTO <202351050735@alunos.newtonpaiva.edu.br>
2026-05-13 21:17:21 -0300

    mensagem

--- src/components/AlunoItem.tsx
+++ src/components/AlunoItem.tsx
@@ -5,13 +5,13 @@ type Props={aluno:Aluno,onRemover:(id:number)=>void}
 
 export function AlunoItem({aluno,onRemover}:Props){
     return(
-        <View>
+        <View style={styles.card}>
             <View>
-                <Text></Text>
-                <Text></Text>
+                <Text style={styles.nome}>{aluno.nome}</Text>
+                <Text style={styles.email}>{aluno.email}</Text>
             </View>
-            <TouchableOpacity>
-                <Text></Text>
+            <TouchableOpacity onPress={() => onRemover(aluno.id)}>
+                <Text style={styles.remover}>Remover</Text>
             </TouchableOpacity>
         </View>
     )
@@ -28,5 +28,5 @@ const styles=StyleSheet.create({
     nome:{fontSize:16, fontWeight:"700"},
     email:{fontSize:16, color:"#cac1c1"},
     remover:{fontWeight:"700", color:"#e40505"},
-    
-})
+
+})  
